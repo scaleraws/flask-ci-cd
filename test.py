017acbece23a1d75d0fb07e456ab793b35eb5f4a
Deepak <scaleraws0703@gmail.com>
2025-11-04 23:52:12 +0530

    changed req versions

--- test.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-def test_root():
-    client = app.test_client()
-    resp = client.get("/")
-    assert resp.status_code == 200
-    assert resp.get_json() == {"message": "Hello from Flask CI/CD!"}
