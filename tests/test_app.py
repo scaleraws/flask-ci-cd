5c0128732451925382d89f090ee0e2c333babd83
Deepak <scaleraws0703@gmail.com>
2025-11-05 00:28:21 +0530

    com

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# Add the project root path so we can import app.py
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 from app import app
 
 def test_root():
